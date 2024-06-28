d0d98ed4ce1388e105b4885329203d5094239b52
Mariq12 <fmlabanda@utpl.edu.ec>
2024-06-28 18:26:09 -0500

    Creación página 7 de cards

--- src/page/Home7.jsx
+++ src/page/Home7.jsx
@@ -1,12 +1,14 @@
 import Home7CardList from "../components/cards/Home7CardList";
-
+import '../styles/home7.css';
 
 const Home7 = () => {
     return (
         <>
-            <div className="container">
-                <Home7CardList />
-            </div>
+            <main className="hom7-main">
+                <div className="hom7-container">
+                    <Home7CardList />
+                </div>
+            </main>
         </>
     );
 }
